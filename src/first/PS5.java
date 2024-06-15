6dcbddc2a6e1a5625e157213076a8080e8c2e83e
ShonNahum <shon304082928@gmail.com>
2024-06-15 23:35:30 +0300

    architect change

---
+++ src/first/PS5.java
@@ -0,0 +1,25 @@
+package first;
+
+public class PS5 {
+	int a;
+	
+	public PS5(int a) {
+		this.a = a;
+	}
+
+	
+	
+	public int multiByThree()
+	{
+		a= a *3; 
+		return a;
+	}
+
+
+	public int multiByFour()
+	{
+		a= a *4; 
+		return a;
+	}
+
+}
